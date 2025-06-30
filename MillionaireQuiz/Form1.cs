02ed207812323d499495220adcf51bbc4a77203b
stoimilovh <hstoimilov@gmail.com>
2025-06-30 10:06:44 +0200

    Add Form1 UI, Question class, and 50:50, Call a Friend for the MillionaireQuiz

---
+++ MillionaireQuiz/Form1.cs
@@ -0,0 +1,186 @@
+﻿namespace MillionaireQuiz
+{
+    public partial class Form1 : Form
+    {
+        List<string> list;
+        Question q;
+        int questionCounter = 0;
+        Button[] buttons;
+        TextBox[] questionOrder;
+        TextBox[] moneyAchieved;
+        string[] allOptions;
+
+        public Form1()
+        {
+            InitializeComponent();
+            list = new List<string> { "Атина", "Белград", "Софија", "Скопје" };
+            q = new Question("Кој е главен град на Република Северна Македонија?", list, 3, "Easy");
+            buttons = new Button[] { button1, button2, button3, button4 };
+            questionOrder = new TextBox[] { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7, textBox8, textBox9, textBox10, textBox11, textBox12, textBox13, textBox14, textBox15 };
+            moneyAchieved = new TextBox[] { textBox16, textBox17, textBox18, textBox19, textBox20, textBox21, textBox22, textBox23, textBox24, textBox25, textBox26, textBox27, textBox28, textBox29, textBox30 };
+            allOptions = new string[] { "A", "B", "C", "D" };            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadQuestion();
+        }
+
+        private void LoadQuestion()
+        {
+            questionTextBox.Text = q.question;
+            button1.Text = q.answers[0];
+            button2.Text = q.answers[1];
+            button3.Text = q.answers[2];
+            button4.Text = q.answers[3];
+
+            foreach (Button btn in buttons)
+            {
+                btn.Enabled = true;
+                btn.Visible = true;
+                btn.BackColor = SystemColors.Control;
+
+                btn.FlatStyle = FlatStyle.Standard;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+            }
+        }
+
+        private async void checkCorrect(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton.Name.Equals("button1") && q.correct == 0)
+            {
+                clickedButton.BackColor = Color.Green;
+                updateScreen();
+            }
+            else if (clickedButton.Name.Equals("button2") && q.correct == 1)
+            {
+                clickedButton.BackColor = Color.Green;
+                updateScreen();
+            }
+            else if (clickedButton.Name.Equals("button3") && q.correct == 2)
+            {
+                clickedButton.BackColor = Color.Green;
+                updateScreen();
+            }
+            else if (clickedButton.Name.Equals("button4") && q.correct == 3)
+            {
+                clickedButton.BackColor = Color.Green;
+                updateScreen();
+            }
+            else
+            {
+                MessageBox.Show("Вашиот одговор е погрешен! Играта е завршена!");
+                await Task.Delay(1000);
+                Application.Exit();
+            }
+            
+        }
+
+        private async void updateScreen()
+        {
+            
+            if (questionCounter < questionOrder.Length)
+            {
+                questionOrder[questionCounter].BackColor = Color.Green;
+                moneyAchieved[questionCounter].BackColor = Color.Green;
+                questionCounter++;
+                await Task.Delay(500);
+                LoadQuestion();
+            }
+            else
+            {
+                MessageBox.Show("Квизот е завршен!");
+                await Task.Delay(5000);
+                Application.Exit();
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int correctAnswer = q.correct;
+            string correctLetter = allOptions[correctAnswer];
+            string friendLetter = "";
+            double chance = 0;
+
+            if (q.difficulty.Equals("Easy"))
+                chance = 0.9;
+            else if (q.difficulty.Equals("Medium"))
+                chance = 0.6;
+            else if (q.difficulty.Equals("Hard"))
+                chance = 0.3;
+
+            if (rand.NextDouble() <= chance)
+            {
+                friendLetter = correctLetter;
+            }
+            else
+            {
+                List<string> wrongOptions = allOptions.Where(opt => opt != correctLetter).ToList();
+                friendLetter = wrongOptions[rand.Next(wrongOptions.Count)];
+            }
+
+            Button clickedButton = (Button)sender;
+
+            if (clickedButton == callFriendButton)
+            {
+                MessageBox.Show("Мислам дека точниот одговор е под " + friendLetter, "Побарај пријател");
+            }
+            else if (clickedButton == askAudienceButton)
+            {
+                MessageBox.Show("Публиката гласа за: " + friendLetter, "Прашај ја публиката");
+            }
+
+            clickedButton.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int correct = q.correct;
+            List<int> all = new List<int> { 0, 1, 2, 3 };
+            List<int> wrong = all.Where(i => i != correct).ToList();
+            Random rand = new Random();
+            List<int> toEliminate = new List<int>();
+
+            if (q.difficulty == "Easy")
+            {
+                toEliminate = wrong.OrderBy(x => rand.Next()).Take(2).ToList();
+            }
+            else if (q.difficulty == "Medium")
+            {
+                int keepWrong = wrong.OrderBy(i => Math.Abs(i - correct)).First();
+                toEliminate = wrong.Where(i => i != keepWrong).ToList();
+            }
+            else // Hard
+            {
+                if (rand.NextDouble() <= 0.2)
+                {
+                    // Risk: eliminate correct + one wrong
+                    int randomWrong = wrong[rand.Next(wrong.Count)];
+                    toEliminate.Add(correct);
+                    toEliminate.Add(randomWrong);
+                }
+                else
+                {
+                    toEliminate = wrong.OrderBy(x => rand.Next()).Take(2).ToList();
+                }
+            }
+
+            // Disable the eliminated buttons
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (!toEliminate.Contains(i))
+                {
+                    buttons[i].FlatStyle = FlatStyle.Flat;
+                    buttons[i].FlatAppearance.BorderSize = 3;
+                    buttons[i].FlatAppearance.BorderColor = Color.Orange;
+                }
+            }
+
+            fiftyfiftyButton.Enabled = false;
+        }
+    }
+}
---
+++ MillionaireQuiz/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillionaireQuiz
+{
+    class Question
+    {
+        public String question;
+        public List<String> answers;
+        public int correct;
+        public String difficulty;
+        
+        public Question(String question, List<String> answers, int correct, String difficulty)
+        {
+            this.question = question;
+            this.answers = answers;
+            this.correct = correct;
+            this.difficulty = difficulty;
+        }
+    }
+}
